330ec6ca766ff5010e1bf5d9df26509c8fdae5fe
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-12-19 08:06:01 +0200

    empty main trans

--- src/context/GlobalCxt.jsx
+++ src/context/GlobalCxt.jsx
@@ -6,10 +6,7 @@ import {AppReducer} from './AddReducer'
 const initialState =  {
 
     transction : [
-      { id: 1, text: 'Flower', amount: -20 },
-      { id: 2, text: 'Salary', amount: 300 },
-      { id: 3, text: 'Book', amount: -10 },
-      { id: 4, text: 'Camera', amount: 150 }
+      
     ]
 }
     
